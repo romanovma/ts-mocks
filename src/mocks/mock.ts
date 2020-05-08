3872736bce05f588375e1de6f6114323f477dc44
Mikhail Romanov <mikhail.romanov@comtravo.com>
2020-05-08 23:21:47 +0200

    add jest support

--- src/mocks/mock.ts
+++ src/mocks/mock.ts
@@ -1,105 +1,99 @@
-import { Setup } from './setup';
+import { Setup } from "./setup";
 
 /** Class for mocking objects/interfaces in Typescript */
 export class Mock<T> {
+  /**
+   * Can be used to define empty methods when using extend
+   * mock.extend({ someMethod: Mock.ANY_FUNC });
+   */
+  public static ANY_FUNC = () => undefined;
 
-    /**
-     * Can be used to define empty methods when using extend
-     * mock.extend({ someMethod: Mock.ANY_FUNC });
-    */
-    public static ANY_FUNC = () => undefined;
+  /** Create mock from a Type */
+  public static of<T>(type: { new (): T }): Mock<T> {
+    return new Mock<T>(new type());
+  }
 
-    /** Create mock from a Type */
-    public static of<T>(type: { new(): T }): Mock<T> {
-        return new Mock<T>(new type());
+  public static static<T, K extends keyof T & string>(
+    obj: T,
+    key: K,
+    stub: T[K] & Function
+  ): void {
+    jest.spyOn(obj as any, key).mockImplementation(stub as any);
+    (obj[key] as any).mockClear(); // tslint:disable-line:no-unsafe-any
+  }
+
+  private _object: T = <T>{};
+  private _spies: Map<string, () => jest.SpyInstance> = new Map<
+    string,
+    () => jest.SpyInstance
+  >();
+
+  constructor(
+    object: Partial<{ [key in Extract<keyof T, string>]: any }> | T = <T>{}
+  ) {
+    this._object = object as T;
+    this.extend(object);
+  }
+
+  /** Return the mocked object */
+  public get Object(): T {
+    return <T>this._object;
+  }
+
+  /** Extend the current mock object with implementation */
+  public extend(
+    object: Partial<{ [key in Extract<keyof T, string>]: any }>
+  ): this {
+    Object.keys(object).forEach((key: Extract<keyof T, string>) => {
+      if (typeof object[key] === typeof Function) {
+        const spy = jest.spyOn(object as any, key);
+        this._spies.set(key, () => spy);
+      }
+    });
+    Object.assign(this._object, object);
+    return this;
+  }
+
+  /**
+   * Setup a property or a method with using lambda style settings.
+   * @param propertyName can be used as a fallback for environments where
+   * dynamic inferring of `propertyName` is not possible.
+   * For example, can be helpful in Wallaby.js test runner.
+   */
+  public setup<TProp>(
+    value: (obj: T) => TProp,
+    propertyName?: string
+  ): Setup<T, TProp> {
+    if (!propertyName) {
+      propertyName = this.getPropertyName(value);
     }
 
-    public static static<T, K extends keyof T>(obj: T, key: K, stub: T[K] & Function): void {
-        const spy = ((jasmine as any).isSpy(obj[key]) ? obj[key] : spyOn(obj as any, key)) as jasmine.Spy;
-        spy.calls.reset();
-        spy.and.callFake(stub as any);
+    let setup = new Setup(this, propertyName);
+    this._spies.set(propertyName, () => setup.Spy);
+    return setup;
+  }
+
+  private getPropertyName<TProp>(value: (obj: T) => TProp): string {
+    return value.toString().match(/(return|=>)\s[\w\d_]*\.([\w\d$_]*)\;?/)[2];
+  }
+
+  /**
+   * Get the spy of method or property that has be set with extend of setup/is.
+   * @param propertyName can be used as a fallback for environments where
+   * dynamic inferring of `propertyName` is not possible.
+   * For example, can be helpful in Wallaby.js test runner.
+   */
+  public spyOf<TProp>(
+    value: (obj: T) => TProp,
+    propertyName?: string
+  ): jest.SpyInstance {
+    if (!propertyName) {
+      propertyName = this.getPropertyName(value);
     }
 
-    private _object: T = <T>{};
-    private _spies: Map<string, () => jasmine.Spy> = new Map<string, () => jasmine.Spy>();
-
-    constructor(object: Partial<{ [key in Extract<keyof T, string>]: T[key] }> | T = <T>{}) {
-        this._object = object as T;
-        this.extend(object);
+    if (this._spies.has(propertyName)) {
+      return this._spies.get(propertyName)();
     }
-
-    /** Return the mocked object */
-    public get Object(): T {
-        return <T>this._object;
-    }
-
-    /** Extend the current mock object with implementation */
-    public extend(object: Partial<{ [key in Extract<keyof T, string>]: T[key] }>): this {
-        Object.keys(object).forEach((key: Extract<keyof T, string>) => {
-            if (typeof object[key] === 'function' && !(jasmine as any).isSpy(object[key])) {
-                const spy = spyOn(object as any, key).and.callThrough();
-                this._spies.set(key, () => spy);
-            }
-        });
-        Object.assign(this._object, object);
-        return this;
-    }
-
-    /** 
-     * Setup a property or a method with using lambda style settings.
-     * @param propertyName can be used as a fallback for environments where 
-     * dynamic inferring of `propertyName` is not possible. 
-     * For example, can be helpful in Wallaby.js test runner.
-     */
-    public setup<TProp>(value: (obj: T) => TProp, propertyName?: string): Setup<T, TProp> {
-        if (!propertyName) {
-            propertyName = this.getPropertyName(value);
-        }
-
-        let setup = new Setup(this, propertyName);
-        this._spies.set(propertyName, () => setup.Spy);
-        return setup;
-    }
-
-    private getPropertyName<TProp>(value: (obj: T) => TProp): string {
-        return value.toString().match(/(return|=>)\s[\w\d_]*\.([\w\d$_]*)\;?/)[2];
-    }
-
-    /** 
-     * Get the spy of method or property that has be set with extend of setup/is.
-     * @param propertyName can be used as a fallback for environments where 
-     * dynamic inferring of `propertyName` is not possible. 
-     * For example, can be helpful in Wallaby.js test runner.
-     */
-    public spyOf<TProp>(value: (obj: T) => TProp, propertyName?: string): jasmine.Spy {
-        if (!propertyName) {
-            propertyName = this.getPropertyName(value);
-        }
-
-        if (this._spies.has(propertyName)) {
-            return this._spies.get(propertyName)();
-        }
-        return undefined;
-    }
-
-    /**
-     * Get the jasmineCalls information of a specify method 
-     * @summary due to @types/jasmine update the jasmine.Calls is not working anymore
-     * therefor the callsOf method now returns any instead of jasmineCalls.  
-     * In runtime this method returns a jasmine.Calls or jasmine.Calls<Fn>
-     */
-    public callsOf<TProp>(value: (obj: T) => TProp, propertyName?: string): any {
-        let spy = this.spyOf(value);
-        return spy ? spy.calls : undefined;
-    }
-
-    /**
-     * Reset the calls count of a specific method
-     */
-    public resetCalls<TProp>(value: (obj: T) => TProp, propertyName?: string): void {
-        let calls = this.callsOf(value)
-        if (calls) {
-            calls.reset();
-        }
-    }
+    return undefined;
+  }
 }
--- src/mocks/setup.ts
+++ src/mocks/setup.ts
@@ -1,36 +1,35 @@
-import { Mock } from './mock';
-import Spy = jasmine.Spy;
+import { Mock } from "./mock";
+import Spy = jest.SpyInstance;
 
 export class Setup<T, TReturn> {
+  private spy: Spy;
 
-    private spy: Spy;
+  constructor(private mock: Mock<T>, private key: string) {
+    mock.Object[key] = <T>{};
+    this.spy = jest.spyOn(mock.Object as any, key as Extract<keyof T, string>);
+  }
 
-    constructor(
-        private mock: Mock<T>,
-        private key: string,
-    ) {
-        mock.Object[key] = <T>{};
-        this.spy = spyOn(mock.Object as any, key as Extract<keyof T, string>);
+  public get Spy() {
+    return this.spy;
+  }
+
+  /** Setup the return value for the setup of the property */
+  public is(value: TReturn): Mock<T> {
+    this.mock.Object[this.key] = value;
+    if (typeof value === "function") {
+      this.spy = jest.spyOn(
+        this.mock.Object as any,
+        this.key as keyof T & string
+      );
     }
+    return this.mock;
+  }
 
-    public get Spy() {
-        return this.spy;
-    }
-
-    /** Setup the return value for the setup of the property */
-    public is(value: TReturn): Mock<T> {
-        this.mock.Object[this.key] = value;
-        if (typeof (value) === 'function') {
-            this.spy = spyOn(this.mock.Object as any, this.key as keyof T).and.callThrough();
-        }
-        return this.mock;
-    }
-
-    /** 
-     * Specify the return value of the property / method
-     * Can be used when mocking generic methods.
-     */
-    public as<TReturnAs>(): Setup<T, TReturnAs> {
-        return new Setup(this.mock, this.key);
-    }
+  /**
+   * Specify the return value of the property / method
+   * Can be used when mocking generic methods.
+   */
+  public as<TReturnAs>(): Setup<T, TReturnAs> {
+    return new Setup(this.mock, this.key);
+  }
 }
